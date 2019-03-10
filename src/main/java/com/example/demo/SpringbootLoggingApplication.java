88dc3e7723dc31b1fec6cf5fef08e9f54d0e2bb7
mudongzi <mudongzi@xudong.com>
2019-03-10 18:03:14 +0800

    新建分支

--- src/main/java/com/example/demo/SpringbootLoggingApplication.java
+++ src/main/java/com/example/demo/SpringbootLoggingApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootLoggingApplication {
 
 	public static void main(String[] args) {
-		System.out.println("测试日志文件");
+		System.out.println("新建分支文件");
 		SpringApplication.run(SpringbootLoggingApplication.class, args);
 	}
 
